e9f856e0d198789732f213e0dc99519421b84139
todilon <120230985+todilon@users.noreply.github.com>
2024-03-19 20:44:46 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,24 +1,18 @@
-# Importa as bibliotecas necessárias do Flask e suas extensões
 from flask import Flask, render_template, session, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField
+from wtforms import StringField, SubmitField, SelectField, PasswordField
 from wtforms.validators import DataRequired
 from datetime import datetime
-from flask_moment import Moment
 
-# Inicializa a aplicação Flask
 app = Flask(__name__)
-
-# Configura uma chave secreta para proteger os dados da aplicação
 app.config['SECRET_KEY'] = 'hard to guess string'
 
-# Inicializa as extensões Flask-Bootstrap e Flask-Moment
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-# Define uma classe de formulário Flask-WTF para a página de formulário
+
 class NameForm(FlaskForm):
     name = StringField('Informe o seu nome', validators=[DataRequired()])
     surname = StringField('Informe o seu sobrenome:', validators=[DataRequired()])
@@ -26,34 +20,38 @@ class NameForm(FlaskForm):
     discipline = SelectField(u'Informe a sua disciplina:', choices=[('dswa5', 'DSWA5'), ('dwba4', 'DWBA4'), ('GPSA5', 'Gestão de projetos')])
     submit = SubmitField('Submit')
 
-# Trata erros 404 (página não encontrada) redirecionando para uma página de erro personalizada
+
+
+class LoginForm(FlaskForm):
+    user = StringField('',  validators=[DataRequired()], render_kw={"placeholder": "Usuário ou e-mail"})
+    password = PasswordField('', validators=[DataRequired()], render_kw={"placeholder": "Informe a sua senha"} )
+    submit = SubmitField('Submit')
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
-# Trata erros 500 (erro interno do servidor) redirecionando para uma página de erro personalizada
+
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-# Rota principal da aplicação, com métodos GET e POST permitidos
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm() # Instancia um formulário da classe NameForm
-    if form.validate_on_submit(): # Verifica se o formulário foi submetido e é válido
-        old_name = session.get('name') # Obtém o nome armazenado na sessão
-        if old_name is not None and old_name != form.name.data: # Verifica se houve alteração no nome
-            flash('Você alterou o seu nome!') # Exibe uma mensagem flash
-        # Armazena os dados do formulário na sessão
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Você alterou o seu nome!')
         session['name'] = form.name.data
         session['surname'] = form.surname.data
         session['institution'] = form.institution.data
         session['discipline'] = form.discipline.data
-        session['remote_addr'] = request.remote_addr; # Obtém o endereço IP do cliente
-        session['host'] = request.host; # Obtém o host do cliente
-        return redirect(url_for('index')) # Redireciona para a mesma página após a submissão do formulário
-
-    # Renderiza o template index.html passando o formulário e os dados da sessão
+        session['remote_addr'] = request.remote_addr;
+        session['host'] = request.host;
+        return redirect(url_for('index'))
+    
     return render_template('index.html', 
                            form=form, 
                            name=session.get('name'), 
@@ -63,4 +61,18 @@ def index():
                            choices=dict(form.discipline.choices),
                            remote_addr=session.get('remote_addr'),
                            remote_host=session.get('host'),
-                           current_time=datetime.utcnow()) # Passa o tempo atual para o template
+                           current_time=datetime.utcnow())
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        session['user'] = form.user.data
+        return redirect(url_for('loginResponse'))
+
+    return render_template('login.html',form=form,
+                        current_time=datetime.utcnow())
+
+@app.route('/login-response', methods=['GET'])
+def loginResponse():
+    return render_template('login-response.html', usuario=session['user'],current_time=datetime.utcnow())
