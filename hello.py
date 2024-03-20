812d4bb1b7b6554fe5d97b1d14e1292536392670
todilon <120230985+todilon@users.noreply.github.com>
2024-03-20 13:12:41 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,37 +6,41 @@ from wtforms import StringField, SubmitField, SelectField, PasswordField
 from wtforms.validators import DataRequired
 from datetime import datetime
 
+# Criação do aplicativo Flask
 app = Flask(__name__)
+# Configuração da chave secreta para proteger as sessões do usuário
 app.config['SECRET_KEY'] = 'hard to guess string'
 
+# Extensões do Flask
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-
+# Definição de um formulário para registro
 class NameForm(FlaskForm):
+    # Campos do formulário
     name = StringField('Informe o seu nome', validators=[DataRequired()])
     surname = StringField('Informe o seu sobrenome:', validators=[DataRequired()])
     institution = StringField('Informe a sua Insituição de ensino:', validators=[DataRequired()])
     discipline = SelectField(u'Informe a sua disciplina:', choices=[('dswa5', 'DSWA5'), ('dwba4', 'DWBA4'), ('GPSA5', 'Gestão de projetos')])
     submit = SubmitField('Submit')
 
-
-
+# Definição de um formulário para login
 class LoginForm(FlaskForm):
+    # Campos do formulário
     user = StringField('',  validators=[DataRequired()], render_kw={"placeholder": "Usuário ou e-mail"})
     password = PasswordField('', validators=[DataRequired()], render_kw={"placeholder": "Informe a sua senha"} )
     submit = SubmitField('Submit')
 
+# Manipuladores de erros personalizados
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
-
 
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-
+# Rota principal para o formulário de registro
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
@@ -44,6 +48,7 @@ def index():
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
             flash('Você alterou o seu nome!')
+        # Armazenamento dos dados do formulário na sessão do usuário
         session['name'] = form.name.data
         session['surname'] = form.surname.data
         session['institution'] = form.institution.data
@@ -63,16 +68,19 @@ def index():
                            remote_host=session.get('host'),
                            current_time=datetime.utcnow())
 
+# Rota para o formulário de login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
+        # Armazenamento do nome de usuário na sessão do usuário
         session['user'] = form.user.data
         return redirect(url_for('loginResponse'))
 
     return render_template('login.html',form=form,
                         current_time=datetime.utcnow())
 
+# Rota para exibir a resposta de login
 @app.route('/login-response', methods=['GET'])
 def loginResponse():
     return render_template('login-response.html', usuario=session['user'],current_time=datetime.utcnow())
